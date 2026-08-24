ce023c5f11503a16a35704ff1eb838ab8eccf8a5
sudenazkocoglu <sudenazkocoglu4@gmail.com>
2026-08-24 12:46:28 +0300

    feat(strings): is_palindrome fonksiyonu eklendi ve test edildi

--- src/strings.py
+++ src/strings.py
@@ -1,3 +1,8 @@
+
 def reverse_string(text: str) -> str:
     return text[::-1]
+
+
+def is_palindrome(text: str) -> bool:
+    return text == text[::-1]
     
--- tests/test_strings.py
+++ tests/test_strings.py
@@ -1,6 +1,13 @@
-from src.strings import reverse_string
+from src.strings import is_palindrome, reverse_string
+
 
 def test_reverse_string() -> None:
     assert reverse_string("merhaba") == "abahrem"
     assert reverse_string("Python") == "nohtyP"
     assert reverse_string("") == ""
+def test_is_palindrome() -> None:
+    assert is_palindrome("kayak") is True
+    assert is_palindrome("radar") is True
+    assert is_palindrome("merhaba") is False
+    assert is_palindrome("") is True
+    
